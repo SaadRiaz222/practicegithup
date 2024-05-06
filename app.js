fbe823bdb095de0ce9c14cd18f6212244bc0c8e2
SaadRiaz222 <saadriazch@gmail.comgit clone https://github.com/SaadRiaz222/saadmern2024.gitgit config --listgit statusgit clone https://github.com/SaadRiaz222/saadmern2024.gitcd ..Lattech Solution@DESKTOP-L4UOG6O MINGW64 ~git config --global user.email saadriazch@gmail.com>
2024-05-05 20:36:14 -0700

    fdjf kd

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var a,b;
 a=7 ,b=10;
 var res=a+b;
-<<<<<<< HEAD
 console() jkjf dkfj k jdshfd sh jfkd jfkdfdhfk dhf
 
 fdjkf kdhf
@@ -13,6 +12,5 @@ fd fd f
 
 
 f dfd f
-=======
 console() factorial command
->>>>>>> b-2
+ 
